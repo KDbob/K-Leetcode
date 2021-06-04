ec77b7be5bbee526e0f5af82d5788d5cc8f2f49b
Kaide <361122924@qq.com>
2021-06-04 20:01:14 +0800

    create Leetcode/Math 326_power-of-three

--- Leetcode/Math/326_power-of-three/main.cpp
+++ Leetcode/Math/326_power-of-three/main.cpp
@@ -2,14 +2,39 @@
 // Created by kaide on 2021/6/2.
 //
 #include <string>
+#include <math.h>
 
 using namespace std;
 
-bool isPowerOfThree(int n) {
-    string str = to_string(n);
-//    char *p = "256";
-//    int i = stoi(p,0,2);
-    if (n % 10 == 0) {
+/** 验证一个数是否是3的幂次
+ * 《方法一二：循环整除/三进制》
+ * @param n 数字
+ * @return y o n
+ */
+bool isPowerOfThree1(int n) {
+    if (n < 1) return false;
+    while (n % 3 == 0) {
+        n = n / 3;
+    }
+    return n == 1;
+}
+
+/** 《方法三：对数公式》
+ * @param n
+ * @return
+ */
+bool isPowerOfThree3(int n) {
+    if (n < 1) return false;
+    double a = log10(n) / log10(3);
+    return a == floor(a);
+}
+
+/** 《方法四：利用整数限制》
+ * @param n 数字
+ * @return y o n
+ */
+bool isPowerOfTree3(int n) {
+    if (1162261467 % n == 0) {
         return true;
     } else {
         return false;
@@ -18,9 +43,15 @@ bool isPowerOfThree(int n) {
 
 
 int main() {
-    if (isPowerOfThree(27)) {
-        printf("is power of three\n");
+    if (isPowerOfThree1(27)) {
+        printf("is power of three\n");  // expect
     } else {
         printf("not power of three\n");
+    }
+
+    if (isPowerOfThree3(36)) {
+        printf("is power of three\n");
+    } else {
+        printf("not power of three\n"); // expect
     }
 }
